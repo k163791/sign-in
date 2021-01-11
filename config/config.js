a62584bfc4d4e4f613a454d00781f56c23b649ce
k163791 <k163791@nu.edu.pk>
2021-01-12 01:20:08 +0500

    initial & final commit for bable and webpack config

---
+++ config/config.js
@@ -0,0 +1,8 @@
+const config = {
+    env: process.env.NODE_ENV || 'development',
+    port: process.env.POST || 3000,
+    jwtSecret: process.env.JWT_SECRET || "secret",
+    mongoUri: process.env.MONGODB_URI || process.env.MONGODB_HOST || 'mongodb://' + (process.env.IP || 'localhost') + ':' + '/mernproject',
+}
+
+export default config
